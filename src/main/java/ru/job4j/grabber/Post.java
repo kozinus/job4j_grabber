8c316ebdf9124e5ce29543127b07a1c3cf3fac9d
kozinus <kkozin2403@gmail.com>
2023-08-21 16:54:27 +0300

    2.2. Модель данных - Post. [#285211 #438502]

---
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -0,0 +1,45 @@
+package ru.job4j.grabber;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Post {
+    private int id;
+    private String title;
+    private String link;
+    private String description;
+    private LocalDateTime created;
+
+    public Post(int id, String link) {
+        this.id = id;
+        this.link = link;
+        this.created = LocalDateTime.now();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Post post)) {
+            return false;
+        }
+        return id == post.id && link.equals(post.link) && created.equals(post.created);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, link, created);
+    }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "id=" + id
+                + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", created=" + created
+                + '}';
+    }
+}
