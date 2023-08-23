6457e37dfc001dcacc19fa3698b5e042ed65a623
kozinus <kkozin2403@gmail.com>
2023-08-23 20:57:59 +0300

    2.4. HabrCareerParse [#285213]

--- src/main/java/ru/job4j/grabber/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/HabrCareerParse.java
@@ -1,25 +1,28 @@
 package ru.job4j.grabber;
 
-import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import ru.job4j.grabber.utils.DateTimeParser;
-import ru.job4j.grabber.utils.HabrCareerDateTimeParser;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
-public class HabrCareerParse {
-
+public class HabrCareerParse implements Parse {
     private static final String SOURCE_LINK = "https://career.habr.com";
 
-    private static final String PAGE_LINK = String.format("%s/vacancies/java_developer?page=", SOURCE_LINK);
+    private static DateTimeParser dateTimeParser;
 
-    public static String retrieveDescription(String link) throws IOException {
+    public HabrCareerParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
+    }
+
+    public static String retrieveDescription(Document document) {
         StringBuilder out = new StringBuilder();
-        Connection connection = Jsoup.connect(link);
-        Document document = connection.get();
         document.select(".vacancy-description__text").first().getAllElements().forEach(tag -> {
             switch (tag.tagName()) {
                 case "h3":
@@ -36,20 +39,44 @@ public class HabrCareerParse {
         return out.toString();
     }
 
-    public static void main(String[] args) throws IOException {
-        for (int i = 1; i <= 5; i++) {
-            Connection connection = Jsoup.connect(String.format("%s%s", PAGE_LINK, i));
-            Document document = connection.get();
+    public String retrieveTitle(Document document) {
+        return document.select(".page-title__title").first().text();
+    }
+
+    public LocalDateTime retrieveCreated(Document document) {
+        Element row = document.select(".vacancy-header__date").first().child(0).child(0);
+        return dateTimeParser.parse(row.attr("datetime"));
+    }
+
+    private List<Post> pageParsing(String link) {
+        List<Post> posts = new ArrayList<>();
+        try {
+            Document document = Jsoup.connect(link).get();
             Elements rows = document.select(".vacancy-card__inner");
             rows.forEach(row -> {
-                Element titleElement = row.select(".vacancy-card__title").first();
-                Element dateElement = row.select(".vacancy-card__date").first().child(0);
-                Element linkElement = titleElement.child(0);
-                String vacancyName = titleElement.text();
-                DateTimeParser parser = new HabrCareerDateTimeParser();
-                String link = String.format("%s%s", SOURCE_LINK, linkElement.attr("href"));
-                System.out.printf("%s %s %s%n", parser.parse(dateElement.attr("datetime")), vacancyName, link);
+                try {
+                    Document postDoc = Jsoup.connect(String.format("%s%s", SOURCE_LINK,
+                            row.select(".vacancy-card__title").first().child(0).attr("href"))).get();
+                    posts.add(new Post(1, retrieveTitle(postDoc), String.format("%s%s", SOURCE_LINK,
+                            row.select(".vacancy-card__title").first().child(0).attr("href")), retrieveDescription(postDoc),
+                            retrieveCreated(postDoc)));
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             });
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+        return posts;
+    }
+
+    @Override
+    public List<Post> list(String link) {
+        int id = 1;
+        List<Post> posts = new ArrayList<>();
+        for (int i = 1; i <= 5; i++) {
+            posts.addAll(Objects.requireNonNull(pageParsing(String.format("%s%s", link, i))));
+        }
+        return posts;
     }
 }
