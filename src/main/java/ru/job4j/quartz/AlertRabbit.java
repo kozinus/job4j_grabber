d5aab143f747221bac0dc08746fd7870d80918d3
kozinus <kkozin2403@gmail.com>
2023-08-24 15:52:02 +0300

    1.1. Job c параметрами [#260360 #438241]

--- src/main/java/ru/job4j/quartz/AlertRabbit.java
+++ src/main/java/ru/job4j/quartz/AlertRabbit.java
@@ -6,28 +6,35 @@ import org.quartz.impl.StdSchedulerFactory;
 import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Properties;
+import java.util.*;
 
-import static org.quartz.JobBuilder.*;
-import static org.quartz.TriggerBuilder.*;
-import static org.quartz.SimpleScheduleBuilder.*;
+import static org.quartz.JobBuilder.newJob;
+import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
 
 public class AlertRabbit {
     public static void main(String[] args) {
-        try {
+        Properties cfg = new Properties();
+        try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
+            cfg.load(in);
+            Class.forName(cfg.getProperty("driver_class"));
+            String url = cfg.getProperty("url");
+            String login = cfg.getProperty("username");
+            String password = cfg.getProperty("password");
+            Connection connection = DriverManager.getConnection(url, login, password);
             List<Long> store = new ArrayList<>();
             Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
             scheduler.start();
             JobDataMap data = new JobDataMap();
             data.put("store", store);
+            data.put("connection", connection);
             JobDetail job = newJob(Rabbit.class)
                     .usingJobData(data)
                     .build();
             SimpleScheduleBuilder times = simpleSchedule()
-                    .withIntervalInSeconds(5)
+                    .withIntervalInSeconds(Integer.parseInt(cfg.getProperty("rabbit.interval")))
                     .repeatForever();
             Trigger trigger = newTrigger()
                     .startNow()
@@ -50,21 +57,17 @@ public class AlertRabbit {
 
         @Override
         public void execute(JobExecutionContext context) {
-            Properties cfg = new Properties();
-            try (InputStream in = AlertRabbit.class.getClassLoader().getResourceAsStream("rabbit.properties")) {
-                cfg.load(in);
-                Class.forName(cfg.getProperty("driver_class"));
-                String url = cfg.getProperty("url");
-                String login = cfg.getProperty("username");
-                String password = cfg.getProperty("password");
-                Connection connection = DriverManager.getConnection(url, login, password);
-                Statement statement = connection.createStatement();
-                System.out.println("Rabbit runs here ...");
-                List<Long> store = (List<Long>) context.getJobDetail().getJobDataMap().get("store");
-                Long created = System.currentTimeMillis();
+            System.out.println("Rabbit runs here ...");
+            List<Long> store = (List<Long>) context.getJobDetail().getJobDataMap().get("store");
+            Connection connection = (Connection) context.getJobDetail().getJobDataMap().get("connection");
+            Long created = System.currentTimeMillis();
+            try (Statement statement = connection.createStatement()) {
+                String sql = String.format(
+                        "INSERT INTO %s(created_date) VALUES(%d);",
+                        "rabbit", created);
+                statement.execute(sql);
                 store.add(created);
-                statement.execute(String.format("INSERT INTO rabbit(created_date) VALUES (%d)", created));
-            } catch (Exception e) {
+            } catch (SQLException e) {
                 e.printStackTrace();
             }
         }
