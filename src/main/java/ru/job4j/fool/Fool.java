852f75d47b98c929374c568b053a0a12f57d99a2
kozinus <kkozin2403@gmail.com>
2023-09-08 00:14:10 +0300

    2. Принципы Kiss, Dry и Yagni [#238813 #442839]

--- src/main/java/ru/job4j/fool/Fool.java
+++ src/main/java/ru/job4j/fool/Fool.java
@@ -4,26 +4,42 @@ import java.util.Scanner;
 
 public class Fool {
 
-    public boolean isDivByInt(int num, int div) {
+    private boolean isDivByInt(int num, int div) {
         return num % div == 0;
     }
 
-    public static void main(String[] args) {
+    private boolean answerCheck(String out, Scanner io) {
+        boolean success = true;
+        String answer = io.nextLine();
+        if (!out.equals(answer)) {
+            System.out.println("Ошибка. Начинай снова.");
+            success = false;
+        }
+        return success;
+    }
+
+    public void init() {
         System.out.println("Игра FizzBuzz.");
-        Fool fool = new Fool();
         int startAt = 1;
+        boolean turn = false;
         Scanner io = new Scanner(System.in);
         while (startAt < 100) {
             String out = String.format("%s%s",
-                    fool.isDivByInt(startAt, 3) ? "Fizz" : "",
-                    fool.isDivByInt(startAt, 5) ? "Buzz" : "");
+                    this.isDivByInt(startAt, 3) ? "Fizz" : "",
+                    this.isDivByInt(startAt, 5) ? "Buzz" : "");
             out = out.isEmpty() ? String.valueOf(startAt) : out;
-            String answer = io.nextLine();
-            if (!out.equals(answer)) {
-                System.out.println("Ошибка. Начинай снова.");
-                startAt = 0;
+            if (turn) {
+                startAt = answerCheck(out, io) ? startAt : 0;
+            } else {
+                System.out.println(out);
             }
+            turn = !turn;
             startAt++;
         }
+    }
+
+    public static void main(String[] args) {
+        Fool fool = new Fool();
+        fool.init();
     }
 }
