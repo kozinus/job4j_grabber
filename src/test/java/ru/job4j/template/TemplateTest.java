63b99a512059b887518de993ad8bf60ca274ca79
kozinus <kkozin2403@gmail.com>
2023-09-10 17:25:34 +0300

    4. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/TemplateTest.java
@@ -0,0 +1,48 @@
+package ru.job4j.template;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.*;
+
+class TemplateTest {
+
+    @Disabled
+    @Test
+    public void unmatchedKey() {
+        Generator template = new Template();
+        String text = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Kirill");
+        assertThatThrownBy(() -> template.produce(text, map)).
+                isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void extraKey() {
+        Generator template = new Template();
+        String text = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Kirill");
+        map.put("subject", "they");
+        map.put("extra", "third");
+        assertThatThrownBy(() -> template.produce(text, map)).
+                isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    public void doubleKeyMap() {
+        Generator template = new Template();
+        String text = "I am a ${name}, Who are ${subject}? ";
+        Map<String, String> map = new HashMap<>();
+        map.put("name", "Kirill");
+        map.put("subject", "they");
+        assertThat(template.produce(text, map))
+                .isEqualTo("I am a Kirill, Who are they? ");
+    }
+}
