c13521348dba13ad0c74ac13b40a44b3a2d02401
kozinus <kkozin2403@gmail.com>
2023-09-17 20:49:12 +0300

    0. Принцип открытости закрытости [#4914]

--- src/main/java/ru/job4j/ood/srp/ExampleClass.java
+++ src/main/java/ru/job4j/ood/srp/ExampleClass.java
@@ -5,24 +5,24 @@ import ru.job4j.gc.Person;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
-public class ExampleClass implements Example {
+public class ExampleClass {
     List<Person> people = new ArrayList<>();
 
-    @Override
-    public List<Person> personsGenerator(int size) {
+    public List<Person> personsGeneratorRandom(int size) {
         List<Person> generated = new ArrayList<>();
         Random random = new Random();
         generated.add(new Person(random.nextInt(), "Person"));
         return generated;
     }
 
-    @Override
-    public void print() {
-        System.out.println(people);
-    }
-
-    @Override
-    public void sort() {
+    public List<Person> personsGeneratorRandomWithFilter(int size) {
+        List<Person> generated = new ArrayList<>();
+        Random random = new Random();
+        Predicate<Person> filter = x -> true;
+        generated.add(new Person(random.nextInt(), "Person"));
+        return generated.stream().filter(filter).collect(Collectors.toList());
     }
 }
